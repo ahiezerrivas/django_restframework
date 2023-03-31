be082a6460389bbc9fb71d8d87c6ad1d05f3485b
ahiezer <ahiezerrivas@gmail.com>
2023-03-31 18:15:28 -0400

    Clase 29 logout + tokens +sesiones

--- ecommerce_rest/apps/users/views.py
+++ ecommerce_rest/apps/users/views.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from django.contrib.sessions.models import Session
 
 from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework .response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -53,3 +54,29 @@ class Login(ObtainAuthToken):
         else:
             return Response({'error': 'Nombre de usuario o contraseña incorrectos'}, status= status.HTTP_400_BAD_REQUEST)
         return Response({'mensaje':'Hola desde response'}, status= status.HTTP_200_OK)
+    
+class Logout(APIView):
+    
+    def get(self,request,*args,**kwargs):
+        try:
+            token = request.GET.get('token')
+            print(token)
+            token = Token.objects.filter(key = token).first()
+
+            if token:
+                user = token.user
+                all_sessions = Session.objects.filter(expire_date__gte = datetime.now())
+                if all_sessions.exists():
+                    for session in all_sessions:
+                        session_data = session.get_decoded()
+                        if user.id == int(session_data.get('_auth_user_id')):
+                            session.delete()
+                token.delete()
+                session_message = 'Sesiones de usuario eliminadas'
+                token_message = 'Token eliminado'
+                return Response({
+                    'token_message': token_message,'session_message'
+:session_message}, status= status.HTTP_200_OK)
+            return Response({'error':'No se ha encontrado un usuario con estas credenciales'}, status = status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response({'error': 'No se ha encontrado tokenn en la peticion'}, status = status.HTTP_409_CONFLICT)
--- ecommerce_rest/ecommerce_rest/urls.py
+++ ecommerce_rest/ecommerce_rest/urls.py
@@ -6,7 +6,7 @@ from rest_framework import permissions
 
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from apps.users.views import Login
+from apps.users.views import Login,Logout
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -27,6 +27,7 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
+    path('Logout/', Logout.as_view(), name  = 'Logout'),
     path('',Login.as_view(), name='Login'),
     path('usuario/', include('apps.users.api.urls')),
     path('products/', include('apps.products.api.routers')),
